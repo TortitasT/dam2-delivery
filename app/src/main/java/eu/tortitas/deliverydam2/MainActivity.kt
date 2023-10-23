50af4b693e2e75f5866eb9be0b01d82399f7a42e
Victor Garcia <victorgf2011@gmail.com>
2023-10-23 21:00:10 +0200

    feat: login page

--- app/src/main/java/eu/tortitas/deliverydam2/MainActivity.kt
+++ app/src/main/java/eu/tortitas/deliverydam2/MainActivity.kt
@@ -6,10 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import eu.tortitas.deliverydam2.login.ui.LoginScreen
 import eu.tortitas.deliverydam2.ui.theme.DeliveryDAM2Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +16,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DeliveryDAM2Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen(name = "Tortitas")
+                    LoginScreen()
                 }
             }
         }
     }
-}
-
-@Composable
-fun LoginScreen(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Login",
-        modifier = modifier
-    )
-
-
 }
---
+++ app/src/main/java/eu/tortitas/deliverydam2/login/ui/LoginScreen.kt
@@ -0,0 +1,88 @@
+package eu.tortitas.deliverydam2.login.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun LoginScreen(modifier: Modifier = Modifier) {
+    var username by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+
+    Box(modifier = modifier
+        .padding(16.dp)
+        .fillMaxSize()) {
+        Surface(modifier = modifier.padding(PaddingValues(16.dp, 64.dp))) {
+            Column(
+                modifier = modifier,
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+
+                Column {
+                    Text(
+                        text = "Hello there!",
+                        modifier = modifier,
+                        style = TextStyle(
+                            fontSize = 32.sp
+                        )
+                    )
+                    Text(
+                        text = "Welcome back",
+                        modifier = modifier,
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+                    )
+                }
+
+                TextField(
+                    label = { Text(text = "Email") },
+                    modifier = modifier.fillMaxWidth(),
+                    value = username, onValueChange = { username = it })
+
+                TextField(
+                    label = { Text(text = "Password") },
+                    modifier = modifier.fillMaxWidth(),
+                    value = password, onValueChange = { password = it })
+
+                TextButton(onClick = {}){
+                    Text(text = "Don’t have an account? Register now!")
+                }
+            }
+        }
+
+        FloatingActionButton(
+            modifier = modifier.align(Alignment.BottomEnd),
+            onClick = { /*TODO*/ }) {
+            Icon(Icons.Filled.Send, "Login")
+        }
+    }
+}
